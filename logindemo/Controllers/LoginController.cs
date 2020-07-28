0d52589cecad4676e7c1ad8d18b43b10f1883d4b
roshaniasodariya@gmail.com <roshaniasodariya@gmail.com>
2020-07-28 14:16:52 +0530

    Add project files.

---
+++ logindemo/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using logindemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace logindemo.Controllers
+{
+    public class LoginController : Controller
+    {
+      
+
+        registeropertation rop = new registeropertation();
+        public IActionResult login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult login([Bind] register reg)
+        {
+            int res = rop.logcheck(reg);
+            if(res==1)
+            {
+                TempData["msg"] = "You are Successfully Login!!";
+            }
+            else
+            {
+                TempData["msg"] = "Username and Password are Wrong!";
+            }
+            return View();
+        }
+    }
+}
---
+++ logindemo/Models/registeropertation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace logindemo.Models
+{
+    public class registeropertation
+    {
+        string connectionString = "Data Source=DMANTRA5106\\MANTRA2005;Initial Catalog=User;Integrated Security=True";
+
+        public int logcheck(register reg)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("login", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Username", reg.Username);
+                cmd.Parameters.AddWithValue("@Password", reg.Password);
+                SqlParameter oblogin = new SqlParameter();
+                oblogin.ParameterName = "@Isvalid";
+                oblogin.SqlDbType = SqlDbType.Bit;
+                oblogin.Direction = ParameterDirection.Output;
+                cmd.Parameters.Add(oblogin);
+                con.Open();
+                int res = Convert.ToInt32(oblogin.Value);
+                con.Close();
+                return res;
+
+            }
+        }
+    }
+}
